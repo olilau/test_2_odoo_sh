f03c46113adb0e176f296b687a4e3b74ad7f1ff4
Olivier Laurent <olilau@gmail.com>
2020-07-08 14:00:43 +0200

    [ADD] lgk_2

---
+++ lgk_2/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'LGK 2',
+    'version': '13.0.1.0',
+    'description': u"""
+""",
+    'author': 'YourCompany',
+    'depends': [
+        'base', 'mail',
+    ],
+    'data': [
+        'data/ir_ui_menu.xml',
+    ],
+    'application': False,
+    'license': 'OPL-1',
+}
