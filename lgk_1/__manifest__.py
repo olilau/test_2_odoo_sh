fc62a1be0f80b66467292ec3ef8e6a9a5af4f0da
Olivier Laurent <olilau@gmail.com>
2020-07-08 13:38:15 +0200

    [ADD] lgk_1

---
+++ lgk_1/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Studio customizations',
+    'version': '13.0.1.0',
+    'description': u"""
+""",
+    'author': 'YourCompany',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'data/ir_model.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_ui_view.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_ui_menu.xml',
+        'data/ir_model_access.xml',
+    ],
+    'application': False,
+    'license': 'OPL-1',
+}
